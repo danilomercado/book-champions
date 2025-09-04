d6e4017dd4cbf3001a0fd504663e8e4228d83773
danilomercado <mercadodanilo213@hotmail.com>
2025-09-04 15:59:10 -0300

    add bd

--- book-champions-server/src/index.js
+++ book-champions-server/src/index.js
@@ -1,8 +1,17 @@
 import express from "express";
+import booksRoutes from "./routes/books.routes.js";
+import { PORT } from "./config.js";
+import { sequelize } from "./db.js";
 
 const app = express();
 
-const port = 3000;
+try {
+  app.listen(PORT);
+  app.use(booksRoutes);
 
-app.listen(port);
-console.log(`Server listening on port ${port}`);
+  await sequelize.sync();
+
+  console.log(`Server listening on port ${PORT}`);
+} catch (error) {
+  console.log(`Ocurrio un error en la inicialización.`);
+}
